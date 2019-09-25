698a19dc0dc93b84de909fd76649fcff381c763d
gpnn <gordon.pn6@gmail.com>
2019-09-25 16:51:43 -0400

    gets courses into a dictionary

--- moodle-scraper.py
+++ moodle-scraper.py
@@ -6,40 +6,8 @@ import configparser
 import datetime
 import logging
 import requests
-from lxml import html
 
 moodle_url = "https://moodle.concordia.ca/moodle/"
-
-
-def get_session():
-    # rewrite with beautifulsoup alternatives
-    session_requests = requests.session()
-    login_url = moodle_url + "login/index.php"
-    result = session_requests.get(login_url)
-
-    soup = BeautifulSoup(result.text, 'html.parser')
-    authenticity_token = soup.find("input", {"name": "logintoken"})['value']
-
-    auth_data = {
-        'logintoken': authenticity_token,
-        'username': username,
-        'password': password
-    }
-
-    result = session_requests.post(
-        login_url,
-        data=auth_data,
-        headers=dict(referer=login_url)
-    )
-
-    url = moodle_url + "my/"
-    result = session_requests.get(
-        url,
-        headers=dict(referer=url)
-    )
-
-    soup = BeautifulSoup(result.text, 'html.parser')
-    return session_requests
 
 
 def get_config():
@@ -59,9 +27,47 @@ def get_config():
     return user, passwd
 
 
+def get_session():
+    session_requests = requests.session()
+    login_url = moodle_url + "login/index.php"
+    result = session_requests.get(login_url)
+
+    soup = BeautifulSoup(result.text, 'html.parser')
+    authenticity_token = soup.find("input", {"name": "logintoken"})['value']
+
+    auth_data = {
+        'logintoken': authenticity_token,
+        'username': username,
+        'password': password
+    }
+
+    result = session_requests.post(
+        login_url,
+        data=auth_data,
+        headers=dict(referer=login_url)
+    )
+
+    return session_requests
+
+
+def get_courses():
+    courses_dict = {}
+    url = moodle_url + "my/"
+    result = session.get(
+        url,
+        headers=dict(referer=url)
+    )
+    soup = BeautifulSoup(result.text, 'html.parser')
+    for header in soup.find_all("h4", {"class": "media-heading"}):
+        courses_dict[header.find("a").string] = header.find("a").get('href')
+
+    return courses_dict
+
+
 if __name__ == '__main__':
     logger = logging.getLogger()
     logger.setLevel(logging.DEBUG)
     logger.addHandler(logging.StreamHandler())
     username, password = get_config()
     session = get_session()
+    courses = get_courses()
